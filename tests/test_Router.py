71eb406764d4deb6ee61778dd7e8a3a8e8f5f12c
balu <balu@mobile>
2020-05-11 21:52:01 +0200

    tryed other test way

--- tests/test_Router.py
+++ tests/test_Router.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest import mock
+from unittest.mock import patch
 from deconzpy import Router
 
 class RouterElementTest(unittest.TestCase):
@@ -8,16 +9,16 @@ class RouterElementTest(unittest.TestCase):
 	def mock_pass(self):
 		pass
 
+	@patch("Router.__loadAllSensors")
+	@patch("Router.__loadAllRules")
+	@patch("Router.__loadAllLights")
+	@patch("Router.__loadAllGroups")
 	def test_Router_init(self):
-		with mock.patch.object(Router, '_Router__loadAllSensors', self.mock_pass) as mRouter:
-			with mock.patch.object(mRouter, '_Router__loadAllRules', self.mock_pass) as mRouter:
-				with mock.patch.object(mRouter, '_Router__loadAllLights', self.mock_pass) as mRouter:
-					with mock.patch.object(mRouter, '_Router__loadAllGroups', self.mock_pass) as Router:
-						r = Router()
-						r2 = Router()
-						self.assertEqual(r, r2) #Router is a sigleton and should be the same object always
-						r3 = Router()
-						self.assertEqual(r3, r2)#Router is a sigleton and should be the same object always
+		r = Router()
+		r2 = Router()
+		self.assertEqual(r, r2) #Router is a sigleton and should be the same object always
+		r3 = Router()
+		self.assertEqual(r3, r2)#Router is a sigleton and should be the same object always
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
