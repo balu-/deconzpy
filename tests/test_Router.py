04baea4ced78ff86747841602fd8dcd3be1eb092
balu <balu@mobile>
2020-05-12 20:36:52 +0200

    fixed Test

--- tests/test_Router.py
+++ tests/test_Router.py
@@ -1,27 +1,30 @@
 import unittest
 from unittest import mock
 from unittest.mock import patch
+from unittest.mock import MagicMock
 from deconzpy import Router
 
-# no groups mock
-def mock_pass(self):
-	pass
-
-Router.__loadAllSensors = mock_pass
-Router.__loadAllRules = mock_pass
-Router.__loadAllLights = mock_pass
-Router.__loadAllGroups = mock_pass
 
 class RouterElementTest(unittest.TestCase):
 
+	def getRouter(self,mock=None):
+		if mock == None:
+			mock = MagicMock(return_value=None)
+		with patch('deconzpy.Router._Router__loadAllSensors', mock):
+			with patch('deconzpy.Router._Router__loadAllRules', mock):
+				with patch('deconzpy.Router._Router__loadAllLights', mock):
+					with patch('deconzpy.Router._Router__loadAllGroups', mock):
+						#seems to hard to mock or i'm not bright enough
+						r = Router()
+						return r
+
+
 	def test_Router_init(self):
-		pass
-		#seems to hard to mock or i'm not bright enough
-		#r = Router()
-		#r2 = Router()
-		#self.assertEqual(r, r2) #Router is a sigleton and should be the same object always
-		#r3 = Router()
-		#self.assertEqual(r3, r2)#Router is a sigleton and should be the same object always
+		#pass
+		r = self.getRouter()
+		r2 = self.getRouter()
+		self.assertEqual(r, r2) #Router is a sigleton and should be the same object always
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
